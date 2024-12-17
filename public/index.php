9223e9af9797dd27fdcabf60252b00ad1b6aa9f3
Justin  M. DeCastro <115455682+learnFromQwerty@users.noreply.github.com>
2024-12-17 23:16:34 +0800

    Revert "404"
    This reverts commit ff23d96acb4f4a74e5817599d383b8e772c7d47b.

--- public/index.php
+++ public/index.php
@@ -37,7 +37,7 @@
     <div class="error-container">
         <div class="error-code">404</div>
         <div class="error-message">Oops! The page you are looking for does not exist.</div>
-        
+        <a href="/" class="btn btn-primary home-btn">Go Back to Home</a>
     </div>
 
     <!-- Bootstrap JS -->
